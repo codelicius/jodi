ab870f9f84c5abc8564e371a535bfaa1b9ea5631
abay <abaywashere@gmail.com>
2017-06-01 09:26:48 +0700

    set error in wevbiew

--- app/src/main/java/makasa/dapurkonten/jodohideal/webView.java
+++ app/src/main/java/makasa/dapurkonten/jodohideal/webView.java
@@ -2,12 +2,14 @@ package makasa.dapurkonten.jodohideal;
 
 import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -109,10 +111,36 @@ public class webView extends AppCompatActivity {
             }
             return true;
         }
+        ProgressDialog pd = new ProgressDialog(webView.this);
+
+        public void onReceivedError(final WebView view, int errorCode, String description, String failingUrl) {
+            AlertDialog.Builder adb = new AlertDialog.Builder(webView.this);
+            adb.setTitle("Warning");
+            adb.setMessage("Tidak dapat terhubung "+view.getUrl());
+            adb.setPositiveButton("Coba Lagi", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    view.reload();
+                }
+            });
+            adb.setNegativeButton("Tutup", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent x = new Intent(webView.this,MainActivity.class);
+                    x.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(x);
+                }
+            });
+            AlertDialog ad = adb.create();
+            ad.show();
+        }
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            pd.setMessage("Tunggu Sebentar");
+            pd.show();
             super.onPageStarted(view, url, favicon);
         }
         public void onPageFinished(WebView view, String url) {
+            pd.dismiss();
             getSupportActionBar().setTitle(view.getTitle());
             super.onPageFinished(view, url);
         }
